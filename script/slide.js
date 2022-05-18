b92874bedaccbb09c4670355e8d393e7e5a72a91
O0O00 <48128321+O0O00@users.noreply.github.com>
2022-05-19 02:17:30 +0900

    update some files,

--- script/slide.js
+++ script/slide.js
@@ -2,3 +2,12 @@ function slide() {
 	let value = document.getElementById("slider").value;
 	document.getElementById("resize").style.clipPath = "polygon(0 0," + value +"% 0," + value +"% 100%,0 100%)";
 }
+
+
+window.onload = function() {
+	var agent = navigator.userAgent.toLowerCase();
+
+if (agent.indexOf("chrome") == -1) {
+alert("본 페이지는 크롬 브라우저에 맞추어 제작되었습니다.\n타 브라우저 이용시 페이지가 깨져 보일 수도 있습니다.");
+}
+}
